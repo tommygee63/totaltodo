e777bb68310408c900098f98d16912a8651c63f4
Tommy Gee <100529843+tommygee63@users.noreply.github.com>
2024-11-09 19:59:47 +0000

    update todo preview

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,9 +3,9 @@ import React from 'react';
 export function TodoForm({handleSubmit, handleNameChange, handleItemChange, name, item}) {
     return (
         <form onSubmit={handleSubmit} >
-            <input type='text' value={name} onChange={handleNameChange} placeholder='Name your To do...' required />
+            <input type='text' value={name} onChange={handleNameChange} placeholder='Name your To do...' required autoComplete='off' />
             <br/>
-            <input type='text' value={item} onChange={handleItemChange} placeholder='Add an item...' required />
+            <input type='text' value={item} onChange={handleItemChange} placeholder='Add an item...' required autoComplete='off' />
             <br/>
             <input type='submit' value='Add To Do Item' />
             <br/>
--- src/components/TodoPreview.js
+++ src/components/TodoPreview.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export function TodoPreview({todo}) {
-    return (
+   /* return (
         <div className='todoPreview' >
             <h2>{todo.name}</h2>
             <Link to={todo.id} className='previewLink' >View</Link>
         </div>
+    ) */
+    return (
+        <Link className='todoPreview' to={todo.id} >
+            <h2>{todo.name}</h2>
+            {todo.items.map((item) => {
+                return <p>{item}</p>
+            })}
+        </Link>
     )
 };
